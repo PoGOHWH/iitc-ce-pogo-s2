46124405975f7ed1d47647c30ffe73b590f68abf
Unknown <amla70@gmail.com>
2019-04-18 11:32:26 +0200

    Add cells up to level 6

--- s2check.user.js
+++ s2check.user.js
@@ -833,6 +833,9 @@ function wrapperPlugin(plugin_info) {
 		const selectRow = `
 			<p>{{level}} level of grid to display: <select>
 			<option value=0>None</option>
+			<option value=6>6</option>
+			<option value=7>7</option>
+			<option value=8>8</option>
 			<option value=9>9</option>
 			<option value=10>10</option>
 			<option value=11>11</option>
