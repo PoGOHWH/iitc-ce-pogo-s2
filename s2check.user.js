a830a2bb3e9d97a4cb0dac30790b3045760a17ec
Sayantan Chaudhuri <sayantan.chaudhuri@gmail.com>
2019-11-18 06:01:16 +0000

    Update Pokéstop/Gym name if Wayspot title edited

--- s2check.user.js
+++ s2check.user.js
@@ -2264,7 +2264,7 @@ img.photo,
 					updateCounter('moved', movedPortals);
 				}
 			}
-			if (!pogoItem.name && portal.name) {
+			if (portal.name && pogoItem.name !== portal.name) {
 				pogoData.store[guid].name = portal.name;
 			}
 			return;
